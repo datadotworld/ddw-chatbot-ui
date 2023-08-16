42ea85c09f1f5da863bb87aa05ef810a8fc53217
jackcompton94 <jackcompton94@gmail.com>
2023-08-16 10:12:07 -0500

    moved api service to live

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const local = 'http://127.0.0.1:5000/get_response'
 
 export const askQuestion = async (question, apiKey) => {
     try {
-        const response = await fetch(local, {
+        const response = await fetch(live, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
