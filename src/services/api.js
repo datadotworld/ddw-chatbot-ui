3fae04146cb5241f48aaed5bb170012e6c2ba126
jackcompton94 <jackcompton94@gmail.com>
2023-08-29 17:53:58 -0500

    added proxy for CORS

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ const API_URL = process.env.DW_API_URL || local
 
 export const askQuestion = async (question, apiKey) => {
     try {
-        const response = await fetch(API_URL + '/get_response', {
+        const response = await fetch('/get_response', {
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json',
