a35c909bbc4068fe5b49f05d4dcc8c0cae887fba
jackcompton94 <jackcompton94@gmail.com>
2023-08-29 18:20:39 -0500

    fixed API url

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,12 @@
-const local = 'http://127.0.0.1:5000/get_response'
+const local = 'http://127.0.0.1:5000'
 const API_URL = process.env.DW_API_URL || local
 
 export const askQuestion = async (question, apiKey) => {
     try {
-        const response = await fetch('https://ddw-chatbot-ui-7aef951365b3.herokuapp.com/get_response', {
+        const response = await fetch(API_URL + '/get_response', {
             method: 'POST', 
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `${apiKey}`
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({question})
         })
