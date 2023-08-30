22160283cae0618e26f4f7051021a6d39d170395
jackcompton94 <jackcompton94@gmail.com>
2023-08-29 19:37:33 -0500

    debugging API url

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 const local = 'http://127.0.0.1:5000'
-const API_URL = process.env.DW_API_URL || local
+// const API_URL = process.env.DW_API_URL || local
 
-console.log(process.env.DW_API_URL)
+API_URL = "https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com"
 
-export const askQuestion = async (question, apiKey) => {
+export const askQuestion = async (question) => {
     try {
         const response = await fetch(API_URL + '/get_response', {
             method: 'POST', 
