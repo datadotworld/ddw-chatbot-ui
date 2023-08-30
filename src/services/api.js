12f613946e059b34acb9ec00753a6d8f54bada64
jackcompton94 <jackcompton94@gmail.com>
2023-08-29 19:41:32 -0500

    debugging API url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const local = 'http://127.0.0.1:5000'
 // const API_URL = process.env.DW_API_URL || local
 
-API_URL = "https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com"
+const API_URL = "https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com"
 
 export const askQuestion = async (question) => {
     try {
