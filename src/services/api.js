48ba24475f5696e68746fe495e2830f83f4ba137
jackcompton94 <jackcompton94@gmail.com>
2023-08-29 19:29:06 -0500

    adding console log for debugging

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 const local = 'http://127.0.0.1:5000'
 const API_URL = process.env.DW_API_URL || local
 
+console.log(process.env.DW_API_URL)
+
 export const askQuestion = async (question, apiKey) => {
     try {
         const response = await fetch(API_URL + '/get_response', {
