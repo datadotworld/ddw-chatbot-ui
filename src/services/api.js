79ac17aa4e0162740a091a667fccc55a72eb9e6b
jackcompton94 <jackcompton94@gmail.com>
2023-09-01 08:51:59 -0500

    testing API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 
 const API_URL_2 = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_HEROKU_URL : process.env.REACT_APP_LOCAL_URL
 console.log(API_URL_2)
-console.log(NODE_ENV)
+console.log(process.env.NODE_ENV)
 const API_URL = 'https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com'
 
 export const askQuestion = async (question) => {
