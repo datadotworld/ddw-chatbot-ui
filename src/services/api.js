e929f4923c8dba7a28e050809517995b1b0a2f03
jackcompton94 <jackcompton94@gmail.com>
2023-09-09 12:12:24 -0500

    updated API_URL logic

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_HEROKU_URL : process.env.REACT_APP_LOCAL_URL
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com' : process.env.REACT_APP_LOCAL_URL
 
 export const askQuestion = async (question) => {
     try {
-        const response = await fetch(process.env.REACT_APP_HEROKU_URL + '/get_response', {
+        const response = await fetch(API_URL + '/get_response', {
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json'
