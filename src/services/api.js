865b9c4699a8cebb9a90114496359ae987ab358e
jackcompton94 <jackcompton94@gmail.com>
2023-09-09 13:53:00 -0500

    reset API URL to hard coding

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com' : process.env.REACT_APP_LOCAL_URL
+// const API_URL = process.env.NODE_ENV === 'production' ? 'https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com' : process.env.REACT_APP_LOCAL_URL
+const API_URL = 'https://ddw-chatbot-backend-fe4ba696a1a5.herokuapp.com'
 
 export const askQuestion = async (question) => {
-    console.log(`NODE ENV: ${process.env.NODE_ENV}`)
-    console.log(`API URL: ${API_URL}`)
     try {
         const response = await fetch(API_URL + '/get_response', {
             method: 'POST', 
